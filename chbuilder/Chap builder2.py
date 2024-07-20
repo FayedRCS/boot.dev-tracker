68baf6640d0246818c16642f3d2520bb8f20f99f
FayedRCS <110136955+FayedRCS@users.noreply.github.com>
2024-07-20 02:31:35 +0200

    added comments

--- chbuilder/Chap builder2.py
+++ chbuilder/Chap builder2.py
@@ -1,12 +1,17 @@
 from Chapbuilder import *
 import os
 
+#specify at folder_path end where to create files --> Chapter 8 would be changed to Chapter 9 and so forth after Chapter folder is created
 folder_path = 'c:\\Users\\Fayed\\boot.dev\\Python course tests\\Chapter 8'
 chapter = ""
 
-
+#Sets Array/list to selected amount of generations. code can be changed to user input as well
+# --> int(input()) and then contents_ch(name_of_input_var)
 myList = contents_ch(3)
 
+#This can be improved. Ideally create a loop that creates amount of 'file openings' 
+#it would need to change myList index, assign it to chapter variable. And then update the file path each time. 
+#Lastly execute the file generations
 chapter = myList[0]
 file_path = os.path.join(folder_path, chapter)
 with open(file_path, 'w') as chBuilt:
@@ -22,3 +27,9 @@ file_path = os.path.join(folder_path, chapter)
 with open(file_path, 'w') as chBuilt:
     pass
 
+#Goal for the finished chapter generator, is to input a number. For example 10 in console.
+#Code would then execute, and create 10 chapters, each chapter has a corresponding chapter for the main
+#Hence creating 20 files, numerized in order. 
+#Also create files for challenges. This should be pretty simple
+
+#in V2.0 It should let me select the name for each chapter, then gererate. i.e if i want chapters for 1-7 and then 10-13
--- chbuilder/Chapbuilder.py
+++ chbuilder/Chapbuilder.py
@@ -1,16 +1,20 @@
+#function to take in an amount of chapter files to create
 def contents_ch(num):
    starting = 0
    ch = 81
    myList = []
    
+   #appends smount input to myList, and changes the name of the .py file accordingly
    while starting < num:
       myList.append(f"ch{ch}.py")
       ch += 1
       starting += 1
    
-      
-      
-   
+   #returning myList only
    return myList
    
-print(contents_ch(3))
+
+   #Fields of improvements here: 
+   #Create a copy of each python file, and put a char = "m", at the end, but before .py
+   #So that the list created looks like this : [ch1.py, ch1m.py, ch2.py, ch2m.py] etc.
+   #Conditions need to be specified
