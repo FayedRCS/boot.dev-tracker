318392bf93fc7e2420659f3ecf189005fa3fa213
FayedRCS <110136955+FayedRCS@users.noreply.github.com>
2024-07-22 02:45:46 +0200

    Finished ch7

--- Chapter 7/ch7Chal3.py
+++ Chapter 7/ch7Chal3.py
@@ -1,2 +1,16 @@
+#def has_enough_gas(gallons_in_car, miles_one_way, miles_per_gallon):
+#    total_miles = gallons_in_car * miles_per_gallon
+#    if total_miles >= miles_one_way*2:
+#        return True
+#    else:
+#        return False
+
+                            #Two ways to solve this problem. Codeblock above is my first though
+                            #Second solution is more tailored to challenge preference
 def has_enough_gas(gallons_in_car, miles_one_way, miles_per_gallon):
-    pass
+    gallons_needed = miles_one_way * 2
+    gallons_available = gallons_in_car * miles_per_gallon
+    if gallons_needed <= gallons_available:
+        return True
+    else:
+        return False
